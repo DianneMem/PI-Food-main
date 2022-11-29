f08aba6021f481b485fd5a31fc7b6194b17988e4
DianneMem <102989674+DianneMem@users.noreply.github.com>
2022-11-29 19:42:50 -0300

    uninstall dotenv

--- client/src/index.js
+++ client/src/index.js
@@ -7,11 +7,10 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './Redux/store';
 import axios from 'axios';
-import dotenv from 'dotenv';
-dotenv.config();
 
 
-axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
+
+axios.defaults.baseURL = 'https://foods-server.onrender.com'|| "http://localhost:3001";
 
 ReactDOM.render(
 
